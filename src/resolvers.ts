c4fc6503a69e52075e56e7e0f97649cad6dc5d90
sa-webb <sawebb30@gmail.com>
2020-01-21 16:32:12 -0600

    cancel and restart resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -50,13 +50,27 @@ export const resolvers: IResolvers = {
         throw new Error();
       }
 
-      const customer = await stripe.customers.create({
-        email: user.email,
-        source,
-        plan: process.env.PLAN
-      });
+      let stripeId = user.stripeId;
 
-      user.stripeId = customer.id;
+      if (!stripeId) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          source,
+          plan: process.env.PLAN
+        });
+        stripeId = customer.id;
+      } else {
+        // update customer
+        await stripe.customers.update(stripeId, {
+          source
+        });
+        await stripe.subscriptions.create({
+          customer: stripeId,
+          plan: process.env.PLAN!
+        })
+      }
+
+      user.stripeId = stripeId;
       user.type = 'test'; // plan nickname
       user.ccLast4 = ccLast4;
 
@@ -79,7 +93,37 @@ export const resolvers: IResolvers = {
       user.ccLast4 = ccLast4;
 
       await user.save();
-      
+
+      return user;
+    },
+    cancelSubscription: async (_, __, { req }) => {
+      if (!req.session || !req.session.userId) {
+        throw new Error('not authenticated');
+      }
+
+      const user = await User.findOne(req.session.userId);
+
+      if (!user || !user.stripeId || user.type !== 'test') {
+        throw new Error();
+      }
+
+      const stripeCustomer = await stripe.customers.retrieve(user.stripeId);
+
+      const [subscription] = stripeCustomer.subscriptions.data;
+
+      await stripe.subscriptions.del(subscription.id);
+
+      // Delete's card on file if customer cancels subscription
+      stripe.customers.deleteSource(
+        user.stripeId,
+        stripeCustomer.default_source as string
+      );
+
+      // Sets new user type
+      user.type = 'free-trial';
+      // user.stripeId = null;
+      await user.save();
+
       return user;
     }
   }
