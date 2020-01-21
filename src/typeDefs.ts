296b5d38511126b2e0f0724115f83be721ca78e0
sa-webb <sawebb30@gmail.com>
2020-01-21 14:44:57 -0600

    change card tDefs

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -5,15 +5,17 @@ export const typeDefs = gql`
     id: ID!
     email: String!
     type: String!
+    ccLast4: String
   }
 
   type Query {
     me: User
   }
-
+  # source: customerID
   type Mutation {
     register(email: String!, password: String!): Boolean!
     login(email: String!, password: String!): User
-    createSubscription(source: String!): User
+    createSubscription(source: String!, ccLast4: String!): User
+    changeCreditCard(source: String!, ccLast4: String!): User
   }
 `;
